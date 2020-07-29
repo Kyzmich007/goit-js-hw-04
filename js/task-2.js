7ea87baa0650dcc96595cfb1ed182182b8232e87
Vladyslav Kuz'menko <Kyzmich007@ukr.net>
2020-07-29 20:48:43 +0300

    amendment

--- js/task-2.js
+++ js/task-2.js
@@ -12,8 +12,7 @@ const inventory = {
 };
 const invokeInventoryAction = function (itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  action = action.bind(inventory);
-  action(itemName);
+  action.call(inventory, itemName);
 };
 
 invokeInventoryAction("Medkit", inventory.add);
